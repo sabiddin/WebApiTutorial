4b8b98c6480c556ef886ae1aead607da40e10f63
sabiddin <abiddinsyed@gmail.com>
2019-09-10 01:02:43 -0400

    Initial repository with a basic controller.

---
+++ WebApiTutorial/WebApiTutorial.Api/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiTutorial.Api.Models;
+
+namespace WebApiTutorial.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private List<ProductModel> products = new List<ProductModel>() {
+            new ProductModel { ProductId = 1, ProductName = "Shirt", Price = 10.00m},
+            new ProductModel { ProductId = 2, ProductName = "Jeans", Price = 15.00m},
+            new ProductModel { ProductId = 3, ProductName = "T-Shirt", Price = 12.00m},
+        };
+        [HttpGet]
+        [Route("GetProducts")]
+        public IActionResult GetProducts() {
+            return Ok(products);
+        }
+    }
+}
---
+++ WebApiTutorial/WebApiTutorial.Api/Models/ProductModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiTutorial.Api.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
